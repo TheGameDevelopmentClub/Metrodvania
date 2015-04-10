0385ca3abd8368be9859791670df98dac8e69b88
Ninjtastic <ryanwoodburn92@gmail.com>
2015-04-09 20:23:28 -0500

    Still adding to level

---
+++ TEST/TEST/Assets/Scripts/switchTPScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class switchTPScript : MonoBehaviour {
+
+	Animator anim;
+	//public movingGrass move;
+	public GameObject obj;
+	public GameObject destination;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent <Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D() {
+		obj.transform.position = destination.transform.position;
+	}
+
+	void OnTriggerStay2D() {
+
+		anim.SetBool("Down", true);
+		//move.Toggle(true); 
+	}
+
+	void OnTriggerExit2D() {
+		
+		anim.SetBool("Down", false);
+	}
+}
