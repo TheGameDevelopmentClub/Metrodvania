bd804dc32b999bf634e0eeaf17906fab52f9f79e
jesusorr <jesus.orrs@gmail.com>
2015-07-10 15:25:51 -0500

    Added alert and attacking stage to FOES
    yes

--- TEST/TEST/Assets/Scripts/foeScript.cs
+++ TEST/TEST/Assets/Scripts/foeScript.cs
@@ -14,11 +14,25 @@ public class foeScript : MonoBehaviour {
 	private float side = 1.0f;
 	private float walkAmount;
 
+    private float alertTimer;
+    private float maxAlertTimer = 1.5f;
+
+
+    //Stages of behavior
+    public bool movingStage;
+    public bool alertStage;
+    public bool attackingStage;
+
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent <Animator>();
 		dead = false;
 
+        movingStage = true;
+        alertStage = false;
+        attackingStage = false;
+
+        alertTimer = 0;
 	}
 	
 	// Update is called once per frame
@@ -30,19 +44,82 @@ public class foeScript : MonoBehaviour {
 			//rigidbody2D.velocity = new Vector2(0, rigidbody2D.velocity.y);
 			return;
 		}
+        ///////////////////////////////
+        // Normal moving. No player near
+        //////////////////////////////
+        if (movingStage)
+        {
 
-		anim.SetBool("Moving", true);
-		walkAmount = side * speed;
-		timer += Time.deltaTime;
+            anim.SetBool("Moving", true);
+            walkAmount = side * speed;
+            timer += Time.deltaTime;
 
-		if (timer > maxTimer) {
-			side = side * -1.0f;
-			timer = 0;
+            if (timer > maxTimer)
+            {
+                side = side * -1.0f;
+                timer = 0;
 
-		} 
+            }
 
-		rigidbody2D.velocity = new Vector2 (walkAmount, rigidbody2D.velocity.y);
-		transform.localScale = new Vector3 (side, 1, 1);
+            rigidbody2D.velocity = new Vector2(walkAmount, rigidbody2D.velocity.y);
+            transform.localScale = new Vector3(side, 1, 1);
+
+            // If player close enough change stage
+            if (Vector2.Distance(transform.position, player.transform.position) <= 6f) {
+                movingStage = false;
+                alertStage = true;
+                attackingStage = false;
+            } 
+        }
+        //////////////////////////////////////////
+        // A moment of realizing the enemy is near
+        //////////////////////////////////////////
+        else if (alertStage) {
+
+            if (player.transform.position.x < this.transform.position.x) {
+                transform.localScale = new Vector3(-1, 1, 1);
+            } else {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            anim.SetBool("Moving", false);
+            alertTimer += Time.deltaTime;
+
+            // Attaking time
+            if (alertTimer > maxAlertTimer) {
+
+                movingStage = false;
+                alertStage = false;
+                attackingStage = true;
+            }
+            
+        }
+        ///////////////////////
+        // Attacking the enemy  
+        //////////////////////
+        else if (attackingStage) {
+
+            anim.SetBool("Moving", true);
+
+            if (player.transform.position.x < this.transform.position.x) {
+
+                rigidbody2D.velocity = new Vector2(-speed*1.5f, rigidbody2D.velocity.y);
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else {
+
+                rigidbody2D.velocity = new Vector2(speed*1.5f, rigidbody2D.velocity.y);
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+
+            //Will chase till player goes away
+            if (Vector2.Distance(transform.position, player.transform.position) > 6f)
+            {
+                movingStage = true;
+                alertStage = false;
+                attackingStage = false;
+            } 
+
+        }
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
